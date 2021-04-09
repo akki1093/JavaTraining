9ba8cdd0332555dbfe9ba7e7f5e27ee32778865d
D1 <D1@TO-9QF6NK2-D.corp.techouts.local>
2021-04-09 13:12:16 +0530

    written logic to validate login

---
+++ UserLogin/src/com/akki/login/ValidateLogin.java
@@ -0,0 +1,23 @@
+package com.akki.login;
+import java.util.Scanner;
+
+public class ValidateLogin {
+
+	public static void main(String[] args) 
+	{
+		Scanner in=new Scanner(System.in);
+		System.out.println("Enter User Name: ");
+		String name=in.nextLine();
+		System.out.println("Enter Password: ");
+		String pswd = in.nextLine();
+		System.out.println("Validating credentials... ");
+		if(name.equals("akki") && pswd.equals("techouts"))
+		{
+			System.out.println("Welcome to Techouts your credentials are valid");
+		}
+		else {
+			System.out.println("Either Username/ password is incorrect.");
+		}
+	}
+
+}
